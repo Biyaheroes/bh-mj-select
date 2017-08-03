c4efe01c65adabe588c51e7684a9cf2f106a90a3
VINSEVINALON\vinsevinalon <vinsevinalon@gmail.com>
2017-08-03 17:37:03 +0800

    Update bh-mj-select component.

---
+++ component.jsx
@@ -0,0 +1,14 @@
+
+"use strict";
+
+import React from "react";
+import ReactDOM from "react-dom";
+
+import Select from "./select.js";
+
+ReactDOM.render( 
+	<Select
+		label="This is a select">
+	</Select>,
+	document.getElementById( "root" ) 
+);
--- test.jsx
+++
@@ -1,9 +0,0 @@
-
-"use strict";
-
-import React from "react";
-import ReactDOM from "react-dom";
-
-import Select from "./select.js";
-
-ReactDOM.render( <Select></Select> );
